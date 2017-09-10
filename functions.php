b36c319a073ab67b21d7ad66ebae9a0d7f82edea
Ken Zirkel <ken@zirkel.com>
2017-09-10 17:09:38 -0400

    remove post-formats

--- functions.php
+++ functions.php
@@ -18,3 +18,28 @@ register_nav_menus(
     )
 );
 
+
+
+function remove_featured_images_from_child_theme() {
+
+    // This will remove support for post thumbnails on ALL Post Types
+    remove_theme_support( 'post-formats' );
+}
+
+add_action( 'after_setup_theme', 'remove_featured_images_from_child_theme', 11 ); 
+
+
+// below not working???
+
+function remove_some_widgets(){
+
+	// Unregister some of the Pinboard sidebars
+	unregister_sidebar( 'Sidebar Bottom' );
+	unregister_sidebar( 'Sidebar Left' );
+	unregister_sidebar( '5' );
+	unregister_sidebar( 'header-1' );
+
+}
+add_action( 'widgets_init', 'remove_some_widgets', 11 );
+
+
